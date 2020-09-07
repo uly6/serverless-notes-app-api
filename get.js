2db3e45ddd17fb8cd476d8bddeb3085199dfcac6
Ulysses Kuntze <kuntze@gmail.com>
2020-09-07 12:21:23 +1200

    test(error): Test handler name error

--- get.js
+++ get.js
@@ -1,9 +1,7 @@
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
-dynamoDb.notExist();
-
-export const main = handler(async (event, context) => {
+export const main2 = handler(async (event, context) => {
   const params = {
     TableName: process.env.tableName,
     // 'Key' defines the partition key and sort key of the item to be retrieved
